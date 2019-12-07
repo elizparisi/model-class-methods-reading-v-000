d7aeaa1e7e0518da388dcb9901f9a8a1b3d25e4c
Liz P <parisielizabeth@gmail.com>
2019-12-07 07:21:59 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,17 @@
 class PostsController < ApplicationController
 
   def index
-  # provide a list of authors to the view for the filter control
-  @authors = Author.all
+    # provide a list of authors to the view for the filter control
+    @authors = Author.all
  
-  # filter the @posts list based on user input
+    # filter the @posts list based on user input
     if !params[:author].blank?
       @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
-    else
-      @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
       end
     else
       # if no filters are applied, show all posts
